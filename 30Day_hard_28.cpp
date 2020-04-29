e5c3f886995f97a151b72205626e3a04baa4625d
2019zhou <52394242+2019zhou@users.noreply.github.com>
2020-04-29 23:25:28 +0800

    Update and rename 30Day_hard_28.py to 30Day_hard_28.cpp

--- 30Day_hard_28.cpp
+++ 30Day_hard_28.cpp
@@ -44,3 +44,34 @@ public:
 // 因为可能遇到负数 l r 中使用max来解决
 // 注意到如果传到上一层节点的时候，能只能传一边，不能传两条边， 所以 return max(l,r) + root->val
 // sum是包含当前节点的一个值
+
+python解法，直接抄过来了
+Solution 1: Helper returning two values: (240 ms, 8 lines)
+
+def maxPathSum(self, root):
+    def maxsums(node):
+        if not node:
+            return [-2**31] * 2
+        left = maxsums(node.left)
+        right = maxsums(node.right)
+        return [node.val + max(left[0], right[0], 0),
+                max(left + right + [node.val + left[0] + right[0]])]
+    return max(maxsums(root))
+My helper function returns two values:
+
+The max sum of all paths ending in the given node (can be extended through the parent)
+The max sum of all paths anywhere in tree rooted at the given node (can not be extended through the parent).
+Solution 2: Helper updating a "global" maximum: (172 ms, 10 lines)
+
+def maxPathSum(self, root):
+    def maxend(node):
+        if not node:
+            return 0
+        left = maxend(node.left)
+        right = maxend(node.right)
+        self.max = max(self.max, left + node.val + right)
+        return max(node.val + max(left, right), 0)
+    self.max = None
+    maxend(root)
+    return self.max
+Here the helper is similar, but only returns the first of the two values (the max sum of all paths ending in the given node). Instead of returning the second value (the max sum of all paths anywhere in tree rooted at the given node), it updates a "global" maximum.
